fa80719f106ea06856e48eaa652d2096879459b1
mANIApHOBIC <git@maniaphobic.org>
2020-05-30 20:14:16 -0700

    Modify step0_repl to use MakeALisp.Step0's functions

--- impls/maniaphobic/step0_repl.hs
+++ impls/maniaphobic/step0_repl.hs
@@ -1,27 +1,4 @@
--- Import the hFlush and stdout symbols for rendering the prompt
-import System.IO (hFlush, stdout)
+import MakeALisp.Step0
 
-replRead = id
-replEval = id
-replPrint s = putStrLn s
-
-{-
-This function implements the REPL loop:
-
-  - Emit the prompt
-  - Flush stdout to render it
-  - Read an expression from stdin
-  - If the line is a ^D, exit. If not, evaluate the expression and repeat the loop
--}
-replLoop = do
-  putStr "user> "
-  hFlush stdout
-  input <- getLine
-  if input == "\EOT"
-  then return ()
-  else do
-    replPrint (replEval (replRead input))
-    replLoop
-
--- `main` is simply the REPL loop
-main = replLoop
+-- `main` is simply `Step0.repl`
+main = MakeALisp.Step0.repl
